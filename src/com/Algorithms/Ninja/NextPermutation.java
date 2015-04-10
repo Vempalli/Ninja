3558cf74b35316d443d0fd85772eff25f18972b3
Phani Theja Swarup Vempalli <pvempa2@uic.edu>
2015-04-09 23:41:32 -0500

    Practice: Next biggest in permutation

---
+++ src/com/Algorithms/Ninja/NextPermutation.java
@@ -0,0 +1,48 @@
+package com.Algorithms.Ninja;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NextPermutation {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		System.out.println("Enter Number");
+		String input = x.nextLine();
+		String output = findNextPermutation(input);
+		System.out.println(output);
+	}
+
+	private static String findNextPermutation(String input) {
+		StringBuffer output = new StringBuffer();
+		//find digit which is less than the next digit
+		char[] chars = input.toCharArray();
+		int n = input.length();
+		int i = 0;
+		for(i = n-1; i >= 0; i--){
+			if(chars[i] > chars[i-1]){
+				break;
+			}
+		}
+		if(i<0){
+			output.append("no answer");
+			return output.toString();
+		}
+		int digitIndex = i-1;
+		int smallIndex = i;
+		int j = 0;
+		for(j= smallIndex+1; j<n; j++){
+			if(chars[j] > chars[digitIndex] && chars[j] < chars[smallIndex]){
+				smallIndex = j;
+			}
+		}
+		//swap
+		char temp = chars[smallIndex];
+		chars[smallIndex] = chars[digitIndex];
+		chars[digitIndex] = temp;
+		
+		Arrays.sort(chars, digitIndex+1, n);
+		return output.append(chars).toString();
+	}
+
+}
